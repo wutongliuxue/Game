88ef2ddf42ea6f2a290fb0f0b0e818bfc25dea31
wutongliuxue <wutongliuxue@163.com>
2022-07-24 11:25:12 -0400

    Add_UI
    -Tong Wu

---
+++ Game_3.0.0_Beta/Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+
+    public float speed;
+    Vector3 startPOS;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPOS = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate((new Vector3(-1, 0, 0)) * speed * Time.deltaTime);
+
+        if(transform.position.x < -24.219)
+        {
+            transform.position = startPOS;
+        }
+    }
+}
